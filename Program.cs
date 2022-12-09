1814aa57b64786c6f2f50365cc502af838783537
Nor_Book <thanawat.srit@kmutt.ac.th>
2022-12-09 16:47:00 +0700

    Edit Program

--- Program.cs
+++ Program.cs
@@ -3,10 +3,11 @@
     static Stack<string> stack = new Stack<string>();
     static void Main(string[] args)
     {
-        int start=0;
-       
+        int start = 0;
+        int count = 0;
+
         Input();
-        if (check(ref start) == 0)
+        if (check(ref count, ref start) == 0)
         {
             Console.WriteLine("Valid");
         }
@@ -17,49 +18,73 @@
     }
     static void Input()
     {
-        int side=0;
+        int side = 0;
         string input;
         while (true)
         {
             input = Console.ReadLine();
-            if(side==0){
+            if (side == 0)
+            {
                 if (input == "(")
                 {
                     stack.Push(input);
                     side++;
                 }
-                else if(input == ")")
+                else if (input == ")")
                 {
                     Console.Clear();
                     Console.WriteLine("Plaese try again");
-                }else{
+                }
+                else
+                {
                     break;
                 }
-            }else{
-                if (input == "("||input == ")")
+            }
+            else
+            {
+                if (input == "(" || input == ")")
                 {
                     stack.Push(input);
-                }else{
+                }
+                else
+                {
                     break;
                 }
             }
         }
     }
 
-    static int check(ref int count)
+    static int check(ref int count, ref int start)
     {
         for (int i = 0; i < stack.GetLength(); i++)
         {
             string p = stack.Pop();
-            if (p == ")")
+            if (start == 0)
             {
-                count++;
-                check(ref count);
+                if (p == ")")
+                {
+                    count++;
+                    start++;
+                    check(ref count, ref start);
+                }
+                else if (p == "(")
+                {
+                    count--;
+                    return count;
+                }
             }
-            else if (p == "(")
+            else
             {
-                count--;
-                check(ref count);
+                if (p == ")")
+                {
+                    count++;
+                    check(ref count, ref start);
+                }
+                else if (p == "(")
+                {
+                    count--;
+                    check(ref count, ref start);
+                }
             }
         }
         return count;
